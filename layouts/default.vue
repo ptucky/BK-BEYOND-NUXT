42cf99693e77840dfdd4f8528746e770bb0ec563
Pravee <w.ptucky@gmail.com>
2020-10-17 19:35:45 +0700

    First upload

---
+++ layouts/default.vue
@@ -0,0 +1,97 @@
+<template>
+  <v-app dark>
+    <v-app-bar
+      absolute
+      color="white"
+      elevate-on-scroll
+      scroll-target="#scrolling-techniques-7"
+    >
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <v-btn
+        icon
+        @click.stop="rightDrawer = !rightDrawer"
+      >
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+    </v-app-bar>
+    
+    <v-sheet
+      id="scrolling-techniques-7"
+      class="overflow-y-auto"
+      max-height="100vh"
+    >
+      <v-container>
+        <v-main class="mt-9">
+          <v-container>
+            <nuxt />
+          </v-container>
+        </v-main>
+      </v-container>
+    </v-sheet>
+
+    <v-navigation-drawer
+      v-model="rightDrawer"
+      :right="right"
+      temporary
+      fixed
+    >
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light>
+              mdi-repeat
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+
+        <v-list>
+            <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+            :to="item.to"
+            router
+            exact
+            >
+            <v-list-item-action>
+                <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+                <v-list-item-title v-text="item.title" />
+            </v-list-item-content>
+            </v-list-item>
+        </v-list>
+
+      </v-list>
+    </v-navigation-drawer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Home',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'BK-BEYOND'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,155 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <div class="text-center">
+        <logo />
+        <vuetify-logo />
+      </div>
+      
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />  
+
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />
+      xxxxxxxx <br />      
+
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
