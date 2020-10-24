7abf4fd654afc96e25b17b8ef689b879df68a32b
Pravee <w.ptucky@gmail.com>
2020-10-24 21:10:34 +0700

    Add Search button and On Click Event

--- layouts/default.vue
+++ layouts/default.vue
@@ -49,10 +49,41 @@
       </v-row>
 
     <v-spacer />
+    
+      <div class="ui-widget">
+        <div style="float:left;">
+          <form @submit.prevent="submitSearch">
+            <input
+              id="searchKeyword"
+              ref="keyword"
+              v-model="keyword"
+              name="keyword"
+              type="text"
+              placeholder="Search"
+              :class="isActiveSearch ? 'showSearch' : 'hideSearch'"
+              @input="changeIcon"
+            >
+          </form>
+        </div>
+        <div style="float:right; width: 30px;">
+          <v-icon
+            :class="isActiveToggleOne ? 'showSearch' : 'hideSearch'"
+            class="search-btn"
+            @click="toggleClassSearch"
+          >
+            mdi-magnify
+          </v-icon>
+          <v-icon
+            :class="isActiveToggleTwo ? 'showSearch' : 'hideSearch'"
+            class="search-btn"
+            style="color: #ffd54f;"
+            @click="submitSearch"
+          >
+            mdi-magnify
+          </v-icon>
+        </div>
+      </div>
 
-      <v-icon class="pl-3">
-        mdi-magnify
-      </v-icon>
       <v-icon 
         @click.stop="rightDrawer = !rightDrawer"
         class="hidden-lg-and-up pl-3"
@@ -102,35 +133,33 @@
           <v-list-item-content>
             <v-list-item-title>
               <v-layout justify-center row class="m-icon-group text-center">
-                
-                  <a href="https://www.facebook.com/bangkokandbeyond" target="_blank" rel="nofollow">
-                     <v-icon
-                        medium
-                        color="blue"
-                        v-ripple
-                      >
-                        mdi-facebook
-                      </v-icon>
-                  </a>
-                  <a href="https://www.instagram.com/bkbeyond/" target="_blank" rel="nofollow">
+                <a href="https://www.facebook.com/bangkokandbeyond" target="_blank" rel="nofollow">
                     <v-icon
-                        medium
-                        color="pink lighten-1"
-                        v-ripple
-                      >
-                        mdi-instagram
-                      </v-icon>
-                  </a>
-                  <a href="https://twitter.com/bk_beyond" target="_blank" rel="nofollow">
-                    <v-icon
-                        medium
-                        color="blue lighten-2"
-                        v-ripple
-                      >
-                        mdi-twitter
-                      </v-icon>
-                  </a>
-             
+                      medium
+                      color="blue darken-1"
+                      v-ripple
+                    >
+                      mdi-facebook
+                    </v-icon>
+                </a>
+                <a href="https://www.instagram.com/bkbeyond/" target="_blank" rel="nofollow">
+                  <v-icon
+                      medium
+                      color="pink lighten-1"
+                      v-ripple
+                    >
+                      mdi-instagram
+                    </v-icon>
+                </a>
+                <a href="https://twitter.com/bk_beyond" target="_blank" rel="nofollow">
+                  <v-icon
+                      medium
+                      color="light-blue lighten-2"
+                      v-ripple
+                    >
+                      mdi-twitter
+                    </v-icon>
+                </a>
               </v-layout>
             </v-list-item-title>
           </v-list-item-content>
@@ -151,6 +180,10 @@ export default {
   data () {
     return {
       loading: false,
+      isActiveSearch: false,
+      isActiveToggleOne: true,
+      isActiveToggleTwo: false,
+      keyword: '',
       mainMenu: [
         {
           icon: 'mdi-apps',
@@ -216,6 +249,37 @@ export default {
     this.handleScrollTopbar(checkDeveice)
   },
   methods: {
+    changeIcon() {
+      if (this.$refs.keyword.value.length >= 1) {
+        this.isActiveToggleOne = false
+        this.isActiveToggleTwo = true
+      } else {
+        this.isActiveToggleOne = true
+        this.isActiveToggleTwo = false
+      }
+    },
+    toggleClassSearch: function () {
+      if (this.isActiveSearch) {
+        this.isActiveSearch = false
+      } else {
+        this.isActiveSearch = true
+      }
+    },
+    submitSearch() {
+      this.$router.push({ path: `/search?key=${this.keyword}` })
+      if (this.keyword !== '') {
+        this.isActiveToggleOne = true
+        this.isActiveToggleTwo = false
+        this.toggleClassSearch()
+
+        if (this.$nuxt.$route.name === 'search') {
+          location.replace(
+            `${process.env.baseUrl}` + `/search?key=${this.keyword}`
+          )
+        }
+        this.keyword = ''
+      }
+    },
     handleScrollTopbar(checkDeveice) {
       window.addEventListener(
         'scroll',
@@ -226,20 +290,19 @@ export default {
 
             document.getElementById('bkbeyond-logo').style.transition = '.2s'
             document.querySelector('.theme--light.v-app-bar.v-toolbar.v-sheet').style.background = '#e2e4eb'
+            document.getElementById('app-bar').style.position = 'fixed'
 
             if (checkDeveice == 'mobile') {
-              document.getElementById('bkbeyond-logo').style.paddingTop = '2px'
-              document.getElementById('bkbeyond-logo').style.width = '90px'
-              document.getElementById('app-bar').style.height = '51px'
-
+              document.getElementById('bkbeyond-logo').style.paddingTop = '5px'
+              document.getElementById('bkbeyond-logo').style.width = '100px'
+              document.getElementById('app-bar').style.height = '52px'
             } else {
-              document.getElementById('bkbeyond-logo').style.paddingTop = '15px'
-              document.getElementById('bkbeyond-logo').style.width = '115px'
+              document.getElementById('bkbeyond-logo').style.paddingTop = '10px'
+              document.getElementById('bkbeyond-logo').style.width = '110px'
               document.getElementById('app-bar').style.transition = '.2s'
               document.getElementById('app-bar').style.marginTop = '-5px'
               document.getElementById('app-bar').style.height = '60px'
-              document.getElementById('app-bar').style.position = 'fixed'
-
+              
               document.getElementById('app-bar-tab').style.transition = '.2s'
               document.getElementById('app-bar-tab').style.height = '60px'
             }
@@ -247,18 +310,17 @@ export default {
           } else {
 
             document.querySelector('.theme--light.v-app-bar.v-toolbar.v-sheet').style.background = 'transparent'
+            document.getElementById('app-bar').style.position = 'relative'
 
             if (checkDeveice == 'mobile') {
-              document.getElementById('bkbeyond-logo').style.paddingTop = '10px'
+              document.getElementById('bkbeyond-logo').style.paddingTop = '15px'
               document.getElementById('bkbeyond-logo').style.width = '105px'
-              document.getElementById('app-bar').style.height = '51px'
-
+              document.getElementById('app-bar').style.height = '52px'
             } else {
               document.getElementById('bkbeyond-logo').style.paddingTop = '15px'
               document.getElementById('bkbeyond-logo').style.width = '140px'
               document.getElementById('app-bar').style.marginTop = '0px'
               document.getElementById('app-bar').style.height = '75px'
-              document.getElementById('app-bar').style.position = 'relative'
 
               document.getElementById('app-bar-tab').style.height = '75px'
             }
