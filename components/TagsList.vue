68728c2fa8195b300602be0c1ff8d64759ff7e6d
pravee inno <pravee@innovisor.co.th>
2020-12-01 13:04:41 +0700

    update

--- components/TagsList.vue
+++ components/TagsList.vue
@@ -1,13 +1,23 @@
 <template>
     <v-row class="row-cover-box" justify="center">
-        <v-col cols="12" sm="8" md="12">
-            <div v-for="(tag, i) in tags" :key="i">
-                <nuxt-link  :to="{ path: pageTitle + '/' + tag.slug }">
-                    <h1 v-if="tag.title" v-text="tag.title" class="text-center"></h1>
+        <v-col cols="12" sm="12" md="12" class="text-center">
+            <h2 class="page-title">
+                <a href="#">{{ pageTitle }}</a>
+            </h2>
+        </v-col>
+        <v-col cols="12" sm="12" md="6"  v-for="(tag, i) in tags" :key="i">
+            <div class="text-left box-content">
+            <h3 class="home-head-title text-titile-link">
+                <nuxt-link :to="{ path: pageTitle + '/' + tag.slug }">
+                <img :src="tag.feature_image" class="img-post">
+                {{ tag.title }}
                 </nuxt-link>
-                <div class="text-justify pt-3">
-                    <div v-if="tag.excerpt" v-html="tag.excerpt"></div>
-                </div>
+            </h3>
+            <p class="pt-2 text-excert" v-if="tag.excerpt" v-text="tag.excerpt.replace(/(<([^>]+)>)|&nbsp;/ig, '').slice(0, 200) + `...`"></p>
+            <v-spacer>
+                <!-- <small class="blue-grey--text text--lighten-2">{{ beautyFullDate(tag.updated_at) }}</small> -->
+                <!-- <small class="blue-grey--text text--lighten-2" v-text="`by ${tag.authors[0].name}`"></small> -->
+            </v-spacer>
             </div>
         </v-col>
     </v-row>
@@ -28,17 +38,24 @@ export default {
     },
     mounted() {
         this.asyncData(this.pageName)
-        //console.log(this.$route.fullPath);
     },
     methods: {
         async asyncData (pageName) {
           const tag = await getTags(pageName)
           this.tags = tag
         },
+        beautyFullDate(val) {
+            let monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+            let date = new Date(val);
+            let fullDate = monthNames[(date.getMonth()+1)] + " " + date.getDate().toString() + ", " + date.getFullYear().toString()
+            return fullDate
+        },
     }
 }
 </script>
 
 <style lang="scss" scoped>
-
+    .img-post{
+        height: 300px;
+    }
 </style>
--- components/TagsListMain.vue
+++ components/TagsListMain.vue
@@ -1,16 +1,27 @@
 <template>
     <v-row class="row-cover-box" justify="center">
-        <v-col cols="12" sm="8" md="12">
-            <div v-for="(tag, i) in tags" :key="i">
-                <nuxt-link  :to="{ path: pageTitle + '/' + tag.slug }">
-                    <h1 v-if="tag.title" v-text="tag.title" class="text-center"></h1>
+        <v-col cols="12" sm="12" md="12" class="text-center">
+            <h2 class="page-title">
+                <a href="#">{{ pageTitle }}</a>
+            </h2>
+        </v-col>
+        <v-col cols="12" sm="12" md="6"  v-for="(tag, i) in tags" :key="i">
+            <div class="text-left box-content">
+            <h3 class="home-head-title text-titile-link">
+                <nuxt-link :to="{ path: pageTitle + '/' + tag.tags[0].slug + '/' + tag.slug }">
+                <img :src="tag.feature_image" class="img-post">
+                {{ tag.title }}
                 </nuxt-link>
-                <div class="text-justify pt-3">
-                    <div v-if="tag.excerpt" v-html="tag.excerpt"></div>
-                </div>
+            </h3>
+            <p class="pt-2 text-excert" v-if="tag.excerpt" v-text="tag.excerpt.replace(/(<([^>]+)>)|&nbsp;/ig, '').slice(0, 200) + `...`"></p>
+            <v-spacer>
+                <!-- <small class="blue-grey--text text--lighten-2">{{ beautyFullDate(tag.updated_at) }}</small> -->
+                <!-- <small class="blue-grey--text text--lighten-2" v-text="`by ${tag.authors[0].name}`"></small> -->
+            </v-spacer>
             </div>
         </v-col>
     </v-row>
+
 </template>
 
 <script>
@@ -35,10 +46,18 @@ export default {
           const tag = await getTagsMain(childTag)
           this.tags = tag
         },
+        beautyFullDate(val) {
+            let monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+            let date = new Date(val);
+            let fullDate = monthNames[(date.getMonth()+1)] + " " + date.getDate().toString() + ", " + date.getFullYear().toString()
+            return fullDate
+        },
     }
 }
 </script>
 
 <style lang="scss" scoped>
-
+    .img-post{
+        height: 300px;
+    }
 </style>
